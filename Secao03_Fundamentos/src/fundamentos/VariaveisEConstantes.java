db0834a2dad78744bf272112d942a683a0d7943c
Jadson <jadsonhenrique2004@gmail.com>
2023-06-21 13:06:48 -0300

    21. Variáveis e Constantes

---
+++ Secao03_Fundamentos/src/fundamentos/VariaveisEConstantes.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class VariaveisEConstantes {
+	
+	public static void main(String[] args) {
+		double raio = 3;
+		final double pi = 3.14;
+		double area = pi * raio * raio;
+		
+		System.out.println("A área da circunferência é " + area + "cm");
+	}
+
+}
