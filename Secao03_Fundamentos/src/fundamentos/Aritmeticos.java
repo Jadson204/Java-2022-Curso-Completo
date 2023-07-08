696a029b33f0bb574b54e36e4fb9b8e71a490fed
Jadson <jadsonhenrique2004@gmail.com>
2023-07-08 11:18:15 -0300

    43. Operadores Aritméticos

---
+++ Secao03_Fundamentos/src/fundamentos/Aritmeticos.java
@@ -0,0 +1,28 @@
+package fundamentos;
+
+public class Aritmeticos {
+	
+	public static void main(String[] args) {
+		
+		 System.out.println(2 + 3);
+		 
+		 var x = 34.56;
+		 double y = 2.2;
+		 
+		 System.out.println(x + y);
+		 System.out.println(x - y);
+		 System.out.println(x * y);
+		 System.out.println(x / y);
+		 
+		 int a = 8;
+		 int b = 3;
+		 
+		 System.out.println(a + b);
+		 System.out.println(a - b);
+		 System.out.println(a * b);
+		 System.out.println(a / b);
+		 System.out.println(a / (double) b);
+		 System.out.println(a % b);
+	}
+
+}
