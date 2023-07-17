6aa7d7da2ed4c237dda0fce0421f37fef2a08d06
Jadson <jadsonhenrique2004@gmail.com>
2023-07-17 15:19:01 -0300

    79. Desafio FOR

---
+++ Secao03_Fundamentos/src/controle/DesafioFor.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class DesafioFor {
+	
+	public static void main(String[] args) {
+		
+		for(String v = "#"; !v.equals("#####"); v += "#") {
+			System.out.println(v);
+		}
+	}
+
+}
