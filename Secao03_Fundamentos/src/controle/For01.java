d6d5094ca6ed3afa9da1effd54b5077c29d870d1
Jadson <jadsonhenrique2004@gmail.com>
2023-07-17 07:59:15 -0300

    71. FOR #01

---
+++ Secao03_Fundamentos/src/controle/For01.java
@@ -0,0 +1,25 @@
+package controle;
+
+public class For01 {
+	
+	public static void main(String[] args) {
+		
+//		int contador = 1;
+//		
+//		while(contador <= 10) {
+//			System.out.printf("i = %d\n", contador);
+//			contador++;
+//		}
+		
+		for(int contador = 1; contador <= 10; contador ++) {
+			System.out.printf("i = %d\n", contador);
+		}
+		
+//		int x = 2;
+//		for(; x < 10;) {
+//			System.out.println("x = " + x);
+//			x++;
+//		}
+	}
+
+}
