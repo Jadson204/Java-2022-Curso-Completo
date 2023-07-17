ce5e1dd34d58f4d0d1e042e4b33d3d8d800e7fd6
Jadson <jadsonhenrique2004@gmail.com>
2023-07-17 10:30:12 -0300

    76. FOR #02

---
+++ Secao03_Fundamentos/src/controle/For02.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class For02 {
+	
+	public static void main(String[] args) {
+		
+//		int contador = 10;
+		
+		for(int contador = 10; contador >= 0; contador -= 2) {
+			System.out.println("i = " + contador);
+		}
+	}
+
+}
