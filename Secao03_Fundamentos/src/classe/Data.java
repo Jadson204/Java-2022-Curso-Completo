525a3f9d4ba5d607572fbb5b5862b3a94bf06f0a
Jadson <jadsonhenrique2004@gmail.com>
2023-07-20 11:08:49 -0300

    102. Desafio Construtor

--- Secao03_Fundamentos/src/classe/Data.java
+++ Secao03_Fundamentos/src/classe/Data.java
@@ -6,6 +6,20 @@ public class Data {
 	int mes;
 	int ano;
 	
+	// Construtores
+	Data() {
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+	
+	Data(int diaInicial, int mesInicial, int anoInicial) {
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;
+	}
+	
+	// Mértodos
 	String dataFormatada() {
 		return String.format("%d/%d/%d", dia, mes, ano);
 	}
--- Secao03_Fundamentos/src/classe/DataTeste.java
+++ Secao03_Fundamentos/src/classe/DataTeste.java
@@ -3,12 +3,14 @@ package classe;
 public class DataTeste {
 
 	public static void main(String[] args) {
-		Data d1 = new Data();
+		Data d1 = new Data();		
+//		d1.dia = 19;
+//		d1.mes = 7;
+//		d1.ano = 2023;
 		
-		d1.dia = 19;
-		d1.mes = 7;
-		d1.ano = 2023;
+		Data d2 = new Data(20, 7, 2023);
 		
-		System.out.printf(d1.dataFormatada());
+		System.out.println(d1.dataFormatada());
+		System.out.println(d2.dataFormatada());
 	}
 }
