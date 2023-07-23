4ce80d02d8a3ab83da1713f300069420c73a076e
Jadson <jadsonhenrique2004@gmail.com>
2023-07-22 23:19:33 -0300

    125. Desafio Array

---
+++ Secao03_Fundamentos/src/arrays/DesafioArray.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class DesafioArray {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Quantas notas: ");
+		int qtdNotas = entrada.nextInt();
+		
+		double[] notas = new double[qtdNotas];
+		
+		for (int i = 0; i < notas.length; i++) {
+			System.out.print("Informe a nota " + (i + 1) + ": ");
+			notas[i] = entrada.nextDouble();
+		}
+		
+		double total = 0;
+		for(double nota: notas) {
+			total += nota;
+		}
+		
+		double media = total/notas.length;
+		System.out.printf("A média é %.2f!!!", media);
+		entrada.close();
+	}
+
+}
